7f95fbac8f4b5683eea1d66f6cbff0e6d0d5b132
Hasnain Ali Rind <130476908+HasnainAliRind@users.noreply.github.com>
2024-10-09 20:27:57 +0500

    Update send_email.js

--- Helpers/send_email.js
+++ Helpers/send_email.js
@@ -1,39 +1,71 @@
-//importing modules
-import nodemailer from 'nodemailer'
-import dotenv from "dotenv"
+// Importing modules
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+import {Resend} from "resend";
 
-dotenv.config()
 
 
-//function to send email to the user
+dotenv.config();
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+// Create a transporter instance using connection pooling
+// const transporter = nodemailer.createTransport({
+//   service: process.env.EMAIL_SERVICE,
+//   host: process.env.EMAIL_HOST,
+//   port: process.env.EMAIL_PORT,
+//   secure: true,
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.EMAILPASS,
+//   },
+// });
+
+// Function to send email to the user
 const sendingMail = async ({ from, to, subject, text }) => {
-
   try {
-    let mailOptions = ({
-      from,
-      to,
-      subject,
-      text
-    })
-   
-    const Transporter = nodemailer.createTransport({
-      service: process.env.EMAIL_SERVICE,
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAILPASS,
-      },
+    // const mailOptions = {
+    //   from,
+    //   to,
+    //   subject,
+    //   text,
+    // };
+
+    // // Wrap sendMail in a Promise
+    // const info = await new Promise((resolve, reject) => {
+    //   transporter.sendMail(mailOptions, (err, info) => {
+    //     if (err) {
+    //       console.error('Error sending email:', err);
+    //       reject(err);
+    //     } else {
+    //       console.log('Email sent:', info.response);
+    //       resolve(info);
+    //     }
+    //   });
+    // });
+
+    // return info; // Optionally return the info object
+    
+
+    const { data, error } = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: to,
+      subject: subject,
+      text: text
     });
+  
+    if (error) {
+      console.log(error);
+      
+    }
+  
+   console.log(data);
+   
 
-    //return the Transporter variable which has the sendMail method to send the mail
-    //which is within the mailOptions
-    return await Transporter.sendMail(mailOptions)
+
   } catch (error) {
-    console.log(error)
+    console.log('Error sending email:', error);
   }
+};
 
-}
-
-export default sendingMail
+export default sendingMail;
